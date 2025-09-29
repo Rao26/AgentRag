301150fb28d22b557b7cdd31f21c76854e5c52fd
Anurag R Rao <126175068+Rao26@users.noreply.github.com>
2025-09-29 21:41:05 +0530

    Delete ARR_WW_PoC/src/tools directory

--- ARR_WW_PoC/src/tools/search_documents.py
+++
@@ -1,5 +0,0 @@
-# src/tools/search_documents.py (optional - can be deleted)
-from langchain.tools import BaseTool
-
-# This file is no longer needed as search is handled in the agent
-# Keeping it for compatibility or can be deleted
